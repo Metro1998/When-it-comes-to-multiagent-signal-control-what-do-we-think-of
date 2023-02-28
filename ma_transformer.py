00e944ac1b6dec3db782fa1ab4ed5c80ee234634
Metro1998 <3170101740@zju.edu.cn>
2023-02-28 16:29:06 +0800

    ma_transformer is coming2

--- ma_transformer.py
+++ ma_transformer.py
@@ -19,32 +19,33 @@ def init_(m, gain=0.01, activate=False):
 
 class SelfAttention(nn.Module):
 
-    def __init__(self, embedding_dim, num_head, num_agent, masked=False):
+    def __init__(self, embd_dim, head_num, agent_num, masked=False):
         super(SelfAttention, self).__init__()
 
-        assert embedding_dim % num_head == 0
+        assert embd_dim % head_num == 0
         self.masked = masked
-        self.n_head = num_head
+        self.head_num = head_num
         # key, query, value projections for all heads
-        self.key = init_(nn.Linear(embedding_dim, embedding_dim))
-        self.query = init_(nn.Linear(embedding_dim, embedding_dim))
-        self.value = init_(nn.Linear(embedding_dim, embedding_dim))
+        self.key = init_(nn.Linear(embd_dim, embd_dim))
+        self.query = init_(nn.Linear(embd_dim, embd_dim))
+        self.value = init_(nn.Linear(embd_dim, embd_dim))
         # output projection
-        self.proj = init_(nn.Linear(embedding_dim, embedding_dim))
+        self.proj = init_(nn.Linear(embd_dim, embd_dim))
         # if self.masked:
         # causal mask to ensure that attention is only applied to the left in the input sequence
-        self.register_buffer("mask", torch.tril(torch.ones(num_agent + 1, num_agent + 1))
-                             .view(1, 1, num_agent + 1, num_agent + 1))
+        self.register_buffer("mask", torch.tril(torch.ones(agent_num + 1, agent_num + 1))
+                             .view(1, 1, agent_num + 1, agent_num + 1))
 
         self.att_bp = None
 
     def forward(self, key, value, query):
         B, L, D = query.size()
+        # B: batch_size, L: sequence_length, D: embedding_dim
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        k = self.key(key).view(B, L, self.n_head, D // self.n_head).transpose(1, 2)  # (B, nh, L, hs)
-        q = self.query(query).view(B, L, self.n_head, D // self.n_head).transpose(1, 2)  # (B, nh, L, hs)
-        v = self.value(value).view(B, L, self.n_head, D // self.n_head).transpose(1, 2)  # (B, nh, L, hs)
+        k = self.key(key).view(B, L, self.head_num, D // self.head_num).transpose(1, 2)  # (B, nh, L, hs)
+        q = self.query(query).view(B, L, self.head_num, D // self.head_num).transpose(1, 2)  # (B, nh, L, hs)
+        v = self.value(value).view(B, L, self.head_num, D // self.head_num).transpose(1, 2)  # (B, nh, L, hs)
 
         # causal attention: (B, nh, L, hs) x (B, nh, hs, L) -> (B, nh, L, L)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
@@ -66,17 +67,17 @@ class SelfAttention(nn.Module):
 class EncodeBlock(nn.Module):
     """ an unassuming Transformer block """
 
-    def __init__(self, embedding_dim, num_head, num_agent):
+    def __init__(self, embd_dim, head_num, agent_num):
         super(EncodeBlock, self).__init__()
 
-        self.ln1 = nn.LayerNorm(embedding_dim)
-        self.ln2 = nn.LayerNorm(embedding_dim)
+        self.ln1 = nn.LayerNorm(embd_dim)
+        self.ln2 = nn.LayerNorm(embd_dim)
         # self.attn = SelfAttention(n_embd, n_head, n_agent, masked=True)
-        self.attn = SelfAttention(embedding_dim, num_head, num_agent, masked=False)
+        self.attn = SelfAttention(embd_dim, head_num, agent_num, masked=False)
         self.mlp = nn.Sequential(
-            init_(nn.Linear(embedding_dim, 1 * embedding_dim), activate=True),
+            init_(nn.Linear(embd_dim, 1 * embd_dim), activate=True),
             nn.GeLU(),  # TODO
-            init_(nn.Linear(1 * embedding_dim, embedding_dim))
+            init_(nn.Linear(1 * embd_dim, embd_dim))
         )
 
     def forward(self, x):
@@ -88,18 +89,18 @@ class EncodeBlock(nn.Module):
 class DecodeBlock(nn.Module):
     """ an unassuming Transformer block """
 
-    def __init__(self, embedding_dim, num_head, num_agent):
+    def __init__(self, embd_dim, head_num, agent_num):
         super(DecodeBlock, self).__init__()
 
-        self.ln1 = nn.LayerNorm(embedding_dim)
-        self.ln2 = nn.LayerNorm(embedding_dim)
-        self.ln3 = nn.LayerNorm(embedding_dim)
-        self.attn1 = SelfAttention(embedding_dim, num_head, num_agent, masked=True)
-        self.attn2 = SelfAttention(embedding_dim, num_head, num_agent, masked=True)
+        self.ln1 = nn.LayerNorm(embd_dim)
+        self.ln2 = nn.LayerNorm(embd_dim)
+        self.ln3 = nn.LayerNorm(embd_dim)
+        self.attn1 = SelfAttention(embd_dim, head_num, agent_num, masked=True)
+        self.attn2 = SelfAttention(embd_dim, head_num, agent_num, masked=True)
         self.mlp = nn.Sequential(
-            init_(nn.Linear(embedding_dim, 1 * embedding_dim), activate=True),
+            init_(nn.Linear(embd_dim, 1 * embd_dim), activate=True),
             nn.GELU(),
-            init_(nn.Linear(1 * embedding_dim, embedding_dim))
+            init_(nn.Linear(1 * embd_dim, embd_dim))
         )
 
     def forward(self, x, rep_encoder):
@@ -107,4 +108,51 @@ class DecodeBlock(nn.Module):
         x = self.ln2(rep_encoder + self.attn2(key=x, value=x, query=rep_encoder))
         x = self.ln3(x + self.mlp(x))
         return x
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, obs_dim, block_num, embd_dim, head_num, agent_num):
+        super(Encoder, self).__init__()
+
+        self.obs_dim  = obs_dim
+        self.embd_dim = embd_dim
+        self.agent_num = agent_num
+
+        self.obs_encoder = nn.Sequential(nn.LayerNorm(obs_dim),
+                                         init_(nn.Linear(obs_dim, embd_dim), activate=True), nn.GELU())
+        self.ln = nn.LayerNorm(embd_dim)
+        self.blocks = nn.Sequential(*[EncodeBlock(embd_dim, head_num, agent_num) for _ in range(block_num)])
+        self.head = nn.Sequential(init_(nn.Linear(obs_dim, embd_dim), activate=True), nn.GELU(), nn.LayerNorm(embd_dim),
+                                  init_(nn.Linear(obs_dim, 1)))
+
+    def forward(self, obs):
+        obs_embeddings = self.obs_encoder(obs)
+        rep = self.blocks(self.ln(obs_embeddings))
+        v_glob = self.head(rep)
+
+        return v_glob, rep
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, obs_dim, action_dim, embedding_dim, block_num, head_num, agent_num, init_log_std):
+        super(Decoder, self).__init__()
+
+        self.action_dim = action_dim
+        self.embedding_dim = embedding_dim
+
+        log_std = torch.zeros(action_dim, ) + init_log_std
+        self.log_std = nn.Parameter(log_std)
+
+        self.action_encoder_dis = nn.Sequential(init_(nn.Linear(action_dim + 1, embedding_dim, bias=False), activate=True),
+                                            nn.GELU())
+        self.action_encoder_con = nn.Sequential(init_(nn.Linear(action_dim, embedding_dim), activate=True), nn.GELU())
+        self.ln = nn.LayerNorm(embedding_dim)
+        self.blocks = nn.Sequential(*[DecodeBlock(embedding_dim, head_num, agent_num) for _ in range(block_num)])
+        self.head = nn.Sequential(init_(nn.Linear(embedding_dim, embedding_dim), activate=True), nn.GELU(), nn.LayerNorm(embedding_dim),
+                                  init_(nn.Linear(embedding_dim, action_dim)))
+
+    def forward(self, action, obs_rep):
+
 
