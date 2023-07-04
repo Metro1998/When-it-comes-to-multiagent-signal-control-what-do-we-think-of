9e2b278d64af4099f3079e82f0b8cab31229e246
Wolfie <3170101740@zju.edu.cn>
2023-07-04 20:29:31 +0800

    main(1/2) todo main/ state(GRU)

--- utils/util.py
+++ utils/util.py
@@ -53,102 +53,70 @@ def map2real(raw_con, max_green):
     return (raw_con + 1) * max_green / 2
 
 
-def autoregressive_act(decoder, obs_rep, env_num, agent_num, action_dim, action_dis, action_con, max_green, device):
-    """
-    In the rollout phase, it will infer the first action a_1 with a start signal a_0, then insert the back into the
-    input and infer the a_2 with [a_0, a_1], and so on till a_n is inferred, aka, autoregressive.
-
-    :param decoder:
-    :param obs_rep:
-    :param env_num:
-    :param agent_num:
-    :param action_dim:
-    :param action_dis:
-    :param action_con: (torch.Tensor) [env_num, agent_num]
-    :param max_green:
-    :param device:
-    :return:
-    """
-
-    # Note that the start signal is [1, 0, 0, ... 0] with the length of action_dim + 1,
-    # that‘s why the 3rd dimension of hybrid_action is action_dim (one hot) + 1 + 1 (raw continuous parameter)
-    hybrid_action = torch.zeros((env_num, agent_num, action_dim + 2), dtype=torch.float32, device=device)
+def autoregressive_act(decoder, obs_rep, batch_size, agent_num, action_dim, action_dis, action_con, agent_to_update, device):
+    hybrid_action = torch.zeros((batch_size, agent_num, action_dim + 2), dtype=torch.float32, device=device)
     hybrid_action[:, 0, 0] = 1
-    output_action = torch.zeros((env_num, agent_num, 2), dtype=torch.float32, device=device)
-    output_action_logp = torch.zeros_like(output_action, dtype=torch.float32, device=device)
 
     for i in range(agent_num):
-        # Get the DISTRIBUTION according to the previous actions
-        logits, means, stds = decoder(hybrid_action, obs_rep)
-        logits = logits[:, i, :]  # TODO check the dimension
-        means = means[:, i, :]
-        stds = stds[:, i, :]
+        with torch.no_grad():
+            logits, means, stds = decoder(hybrid_action, obs_rep)
+            logits = logits[:, i]
+            means = means[:, i]
+            stds = stds[:, i]
 
-        # If it's time to make a decision rather than to infer, just sample the action from the distribution
-        if map2real(action_con[i], max_green) < 0.1:
-            # Mask the last discrete choice, which means the phase is not repeatable
-            logits[action_dis[i]] = float('-inf')
-            dist_dis = Categorical(logits=logits)
-            act_dis = dist_dis.sample()
+            if agent_to_update[:, i]:
+                if action_dis[:, i] >= 0:
+                    logits[action_dis[:, i]] = float('-inf')
+                dist_dis = Categorical(logits=logits)
+                act_dis = dist_dis.sample()
+
+                dist_con = Normal(means[act_dis], stds[act_dis])
+                act_con = dist_con.sample()
+
+            else:
+                dist_dis = Categorical(logits=logits)
+                act_dis = action_dis[:, i]
+
+                dist_con = Normal(means[action_dis], stds[action_dis])
+                act_con = action_con[:, i]
+
             act_logp_dis = dist_dis.log_prob(act_dis)
-
-            dist_con = Normal(means[act_dis], stds[act_dis])
-            act_con = dist_con.sample()
             act_logp_con = dist_con.log_prob(act_con)
 
+            if i + 1 < agent_num:
+                hybrid_action[:, i + 1, 1:-1].copy_(F.one_hot(act_dis, num_classes=action_dim).float())
+                hybrid_action[:, i + 1, -1] = act_con
+
+        if i == 0:
+            output_act_dis = act_dis
+            output_act_con = act_con
+            output_logp_dis = act_logp_dis
+            output_logp_con = act_logp_con
         else:
-            # If it's time to infer, just use the original discrete action and continuous parameter
-            dist_dis = Categorical(logits=logits)
-            act_dis = action_dis[i]
-            act_logp_dis = dist_dis.log_prob(act_dis)
+            output_act_dis = torch.cat((output_act_dis, act_dis), dim=1)
+            output_act_con = torch.cat((output_act_con, act_con), dim=1)
+            output_logp_dis = torch.cat((output_logp_dis, act_logp_dis), dim=1)
+            output_logp_con = torch.cat((output_logp_con, act_logp_con), dim=1)
 
-            dist_con = Normal(means[action_dis], stds[action_dis])
-            act_con = action_con[i]
-            act_logp_con = dist_con.log_prob(act_con)
-
-        if i + 1 < agent_num:
-            hybrid_action[i + 1, 1:-1] = F.one_hot(act_dis, num_classes=action_dim).float()
-            hybrid_action[i + 1, -1] = act_con
-
-        output_action[i, :] = torch.cat((act_dis.float(), act_con), -1)
-        output_action_logp[i, :] = torch.cat((act_logp_dis, act_logp_con), -1)
-
-    return output_action, output_action_logp  # [env_num, agent_num, 2]
+    return output_act_dis, output_logp_dis, output_act_con, output_logp_con
 
 
 def parallel_act(decoder, obs_rep, batch_size, agent_num, action_dim, action_dis, action_con, decision_flag, device):
-    """
-    In the training process, this function get input of sequence [a_0, a_1, ..., a_n-1] and predict [a_1, a_2, ..., a_n]
-    simultaneously in one evaluate_actions process, aka, teaching force.
-
-    :param decoder:
-    :param obs_rep:
-    :param batch_size:
-    :param agent_num:
-    :param action_dim:
-    :param action_dis: (torch.int64) [batch_size, agent_num]
-    :param action_con: [batch_size, agent_num]
-    :param decision_flag: [batch_size, agent_num] to indicate whether it's time to make a decision or to infer,
-    if it's time to make a decision, mask the last discrete choice
-    :param device:
-    :return:
-    """
-    hybrid_action = torch.zeros((batch_size, agent_num, action_dim + 2)).to(device)
+    hybrid_action = torch.zeros((batch_size, agent_num, action_dim + 2), device=device)
     hybrid_action[:, 0, 0] = 1
-    hybrid_action[:, 1:, 1:-1] = F.one_hot(action_dis, num_classes=action_dim)[:, :-1, :]
+    hybrid_action[:, 1:, 1:-1].copy_(F.one_hot(action_dis, num_classes=action_dim)[:, :-1, :])
     hybrid_action[:, 1:, -1] = action_con.unsqueeze(-1)[:, :-1, :]
-    logits, means, stds = decoder(hybrid_action, obs_rep)  # logits: [batch_size, agent_num, action_dim]
+    logits, means, stds = decoder(hybrid_action, obs_rep)
 
-    # mask, if the action_dis is selected before, it shouldn't be selected at this time.
     action_dis_one_hot = F.one_hot(action_dis, num_classes=action_dim)
-    mask = torch.where(decision_flag.unsqueeze(-1), action_dis_one_hot, torch.zeros_like(action_dis_one_hot))
+    mask = decision_flag.unsqueeze(-1).float() * action_dis_one_hot
     logits.masked_fill_(mask, float('-inf'))
     dist_dis = Categorical(logits=logits)
     act_logp_dis = dist_dis.log_prob(action_dis)
     entropy_dis = dist_dis.entropy()
 
-    means = means.gather(-1, action_dis.unsqueeze(-1)).squeeze(-1)  # [batch_size, agent_num]
-    stds = stds.gather(-1, action_dis.unsqueeze(-1)).squeeze(-1)  # [batch_size, agent_num]
+    means = means.gather(-1, action_dis.unsqueeze(-1)).squeeze(-1)
+    stds = stds.gather(-1, action_dis.unsqueeze(-1)).squeeze(-1)
     dist_con = Normal(means, stds)
     act_logp_con = dist_con.log_prob(action_con)
     entropy_con = dist_con.entropy()
