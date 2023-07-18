3cc4dca5ff97dd1d5bccbb9f71cefc818e90f646
Wolfie <3170101740@zju.edu.cn>
2023-07-18 20:34:04 +0800

    when it comes to v1.1 better act

--- utils/util.py
+++ utils/util.py
@@ -175,3 +175,80 @@ def retrieve_eval_results(loc='data/eval_results.pkl'):
         tripinfos['departPos'].append(departPos)
 
     return tripinfos
+
+
+def autoregressive_act_v2(decoder, obs_rep, batch_size, agent_num, action_dim, act_dis_infer, act_con_infer,
+                          agent_to_update, device):
+    hybrid_action = torch.zeros((batch_size, agent_num, action_dim + 2), dtype=torch.float32, device=device)
+    hybrid_action[:, 0, 0] = 1
+    for i in range(agent_num):
+
+        # For agent_i in the batch, there is at least one to update
+        if agent_to_update[:, i].sum() > 0:
+            with torch.no_grad():
+                logits, means, stds = decoder(hybrid_action, obs_rep)
+                logit = logits[:, i]
+                mean = means[:, i]
+                std = stds[:, i]
+                agent_to_update_ = agent_to_update[:, i].bool()
+
+                dist_dis = Categorical(logits=logit)  # Batch discrete distributions
+                act_dis_ = torch.where(agent_to_update_, dist_dis.sample(), act_dis_infer[:, i])
+
+                mean_ = torch.gather(mean, 1, act_dis_.unsqueeze(-1)).squeeze()
+                std_ = torch.gather(std, 1, act_dis_.unsqueeze(-1)).squeeze()
+                dist_con = Normal(mean_, std_)
+                act_con_ = torch.where(agent_to_update_, torch.tanh(dist_con.sample()), act_con_infer[:, i])
+
+                act_logp_dis = dist_dis.log_prob(act_dis_)
+                act_logp_con = dist_con.log_prob(act_con_)
+
+        # For agent_i in the batch, there is no one need to update
+        else:
+            act_dis_ = act_dis_infer[:, i]
+            act_con_ = act_con_infer[:, i]
+            # Padding
+            act_logp_dis = torch.zeros_like(act_dis_, dtype=torch.float32, device=device)
+            act_logp_con = torch.zeros_like(act_con_, dtype=torch.float32, device=device)
+
+        if i + 1 < agent_num:
+            hybrid_action[:, i + 1, 1:-1].copy_(F.one_hot(act_dis_, num_classes=action_dim).float())
+            hybrid_action[:, i + 1, -1] = act_con_
+
+        if i == 0:
+            output_act_dis = act_dis_.unsqueeze(0)
+            output_act_con = act_con_.unsqueeze(0)
+            output_logp_dis = act_logp_dis.unsqueeze(0)
+            output_logp_con = act_logp_con.unsqueeze(0)
+        else:
+            output_act_dis = torch.cat((output_act_dis, act_dis_.unsqueeze(0)), dim=0)
+            output_act_con = torch.cat((output_act_con, act_con_.unsqueeze(0)), dim=0)
+            output_logp_dis = torch.cat((output_logp_dis, act_logp_dis.unsqueeze(0)), dim=0)
+            output_logp_con = torch.cat((output_logp_con, act_logp_con.unsqueeze(0)), dim=0)
+    return torch.transpose(output_act_dis, 0, 1), torch.transpose(output_logp_dis, 0, 1), torch.transpose(
+        output_act_con, 0, 1), torch.transpose(output_logp_con, 0, 1)
+
+
+def parallel_act_v2(decoder, obs_rep, batch_size, agent_num, action_dim, act_dis_exe, act_con_exe, act_dis_infer,
+                    act_con_infer, agent_to_update, device):
+    hybrid_action = torch.zeros((batch_size, agent_num, action_dim + 2), device=device)
+    action_dis = torch.where(agent_to_update.bool(), act_dis_exe, act_dis_infer)
+    action_con = torch.where(agent_to_update.bool(), act_con_exe, act_con_infer)
+    hybrid_action[:, 0, 0] = 1
+    hybrid_action[:, 1:, 1:-1].copy_(F.one_hot(action_dis, num_classes=action_dim)[:, :-1, :])
+    hybrid_action[:, 1:, -1] = action_con[:, :-1]
+    logits, means, stds = decoder(hybrid_action, obs_rep)
+
+    dist_dis = Categorical(logits=logits)
+    act_dis_ = torch.where(agent_to_update.bool(), dist_dis.sample(), act_dis_infer)
+    act_logp_dis = dist_dis.log_prob(act_dis_)[agent_to_update == 1]
+    entropy_dis = dist_dis.entropy()  # todo fix
+
+    means = means.gather(-1, act_dis_.unsqueeze(-1)).squeeze()
+    stds = stds.gather(-1, act_dis_.unsqueeze(-1)).squeeze()
+    dist_con = Normal(means, stds)
+    act_con_ = torch.where(agent_to_update.bool(), torch.tanh(dist_con.sample()), act_con_infer)
+    act_logp_con = dist_con.log_prob(act_con_)[agent_to_update == 1]
+    entropy_con = dist_con.entropy()
+
+    return act_logp_dis, entropy_dis, act_logp_con, entropy_con
